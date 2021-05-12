9387372542e0ca4e68bf8150e811bc926c68a91f
Thuy Nguyen <thuychristinanguyen@gmail.com>
2021-05-12 00:28:21 -0500

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+console.log('Hello from js/scripts.js!');
+
+const select = document.querySelector('select');
+const para = document.querySelector('p');
+
+select.addEventListener('change', setflavor);
+
+function setflavor() {
+  const choice = select.value;
+
+  if (choice === 'watermelon') {
+    para.textContent = 'You save 20%! Here is your coupon code: CACTI20';
+  } else if (choice === 'lime') {
+    para.textContent = 'You save 10% percent! Here is your coupon code: CACTI10';
+  } else if (choice === 'pineapple') {
+    para.textContent = 'You save 15% percent! Here is your coupon code: CACTI15';
+  } else if (choice === 'strawberry') {
+    para.textContent = 'You save 10% percent! Here is your coupon code: CACTI10';
+  } else {
+    para.textContent = '';
+  }
+}
